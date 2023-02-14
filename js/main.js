4657f26285827460a118c49208cba308588f11fc
phuong210 <thuphuongnguyen337@gmail.com>
2023-02-14 16:14:48 +0700

    project Decarlo

--- js/main.js
+++ js/main.js
@@ -144,3 +144,24 @@ const productTab = () => {
     
 }
 productTab();
+
+// slider product detail
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("buy__slider");
+  var dots = document.getElementsByClassName("buy__slider-item");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].style.opacity = "0.5";
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].style.opacity = "1";
+}
